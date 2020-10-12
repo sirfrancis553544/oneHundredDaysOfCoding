c35c90792284dc0fd047c1eabc1725541eef46e6
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-10-12 15:22:20 +0800

    crypto currency api

---
+++ Python/TaskManager/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TaskManager',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Daniel',
+    author_email='',
+    description=''
+)
