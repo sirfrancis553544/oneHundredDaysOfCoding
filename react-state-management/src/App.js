3fc718693d8d856e78dcacab23d3f3aabed4b255
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-10-13 16:41:22 +0800

    movie list

--- react-state-management/src/App.js
+++ react-state-management/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import MovieList from "./MovieList"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <MovieList />
     </div>
   );
 }
--- react-state-management/src/MovieList.js
+++ react-state-management/src/MovieList.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Movie from "./Movie";
 
 const MovieList = () => {
   const [movies, setMovies] = useState([
@@ -18,8 +19,13 @@ const MovieList = () => {
       id: 43422,
     },
   ]);
-
-  return movies.map((movie) => <li>{movie.name}</li>);
+  return (
+    <div>
+      {movies.map((movie) => (
+          <Movie name={movie.name} price={movie.price} key={movie.id}/>
+      ))}
+    </div>
+  );
 };
 
 export default MovieList;
