63bf4607b6c00680b2e14026ed9869457726572f
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-10-16 22:03:49 +0800

    sections

--- react-website-temp/src/components/Button.js
+++ react-website-temp/src/components/Button.js
@@ -1,9 +1,11 @@
-import React from "react";
-import "./Button.css";
+import React from 'react';
+import './Button.css';
 
-const STYLES = ["btn--primary", "btn--outline"];
-const SIZES = ["btn--medium", "btn--large", "btn--mobile", "btn--wide"];
-const COLOR = ["primary", "blue", "red", "green"];
+const STYLES = ['btn--primary', 'btn--outline'];
+
+const SIZES = ['btn--medium', 'btn--large', 'btn--mobile', 'btn--wide'];
+
+const COLOR = ['primary', 'blue', 'red', 'green'];
 
 export const Button = ({
   children,
@@ -11,12 +13,14 @@ export const Button = ({
   onClick,
   buttonStyle,
   buttonSize,
-  buttonColor,
+  buttonColor
 }) => {
   const checkButtonStyle = STYLES.includes(buttonStyle)
     ? buttonStyle
     : STYLES[0];
+
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
   const checkButtonColor = COLOR.includes(buttonColor) ? buttonColor : null;
 
   return (
