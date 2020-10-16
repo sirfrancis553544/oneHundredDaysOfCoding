94476cd2c9631ccdc018db09c50085526c19667b
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-10-16 14:30:02 +0800

    menu

---
+++ react-website-temp/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Router path="/" />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
+// npm start
