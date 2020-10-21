f326906669b632b2ee52ffda8fbdf191f6e3bcc1
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-10-21 23:24:14 +0800

    login.js

--- contactkeeper/client/src/components/auth/Login.js
+++ contactkeeper/client/src/components/auth/Login.js
@@ -1,32 +1,26 @@
 import React, { useState } from "react";
 
-const Register = () => {
+const Login = () => {
   const [user, setUser] = useState({
-    name: "",
     email: "",
     password: "",
-    password2: "",
   });
 
-  const { name, email, password, password2 } = user;
+  const { email, password } = user;
 
   const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("Register submit");
+    console.log("Login submit");
   };
 
   return (
     <div className="form-container">
       <h1>
-        Account <span className="text-primary">Register</span>
+        Account <span className="text-primary">Login</span>
       </h1>
       <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" value={name} onChange={onChange} />
-        </div>{" "}
         <div className="form-group">
           <label htmlFor="email">Email</label>
           <input type="email" name="email" value={email} onChange={onChange} />
@@ -40,18 +34,9 @@ const Register = () => {
             onChange={onChange}
           />
         </div>{" "}
-        <div className="form-group">
-          <label htmlFor="password2">Confirm Password</label>
-          <input
-            type="password"
-            name="password2"
-            value={password2}
-            onChange={onChange}
-          />
-        </div>
         <input
           type="submit"
-          value="Register"
+          value="Login"
           className="btn btn-primary btn-block"
         />
       </form>
@@ -59,4 +44,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
