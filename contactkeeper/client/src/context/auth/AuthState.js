9c6c5053467d524fa79909669f5c6329bac428c1
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-10-27 20:37:04 +0800

    auth

--- contactkeeper/client/src/context/auth/AuthState.js
+++ contactkeeper/client/src/context/auth/AuthState.js
@@ -1,6 +1,9 @@
 import React, { useReducer } from "react";
+import axios from "axios";
 import AuthContext from "./authContext";
 import authReducer from "./authReducer";
+import setAuthToken from "../../utils/setAuthToken";
+
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
@@ -23,14 +26,51 @@ const AuthState = (props) => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   //load user
+  const loadUser = () => async () => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
 
+    try {
+      const res = await axios.get("/api/auth");
+
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({ type: AUTH_ERROR });
+    }
+  };
   //register user
+  const register = async (formData) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const res = await axios.post("/api/users", formData, config);
+
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data,
+      });
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: err.response.data.msg,
+      });
+    }
+  };
 
   //login user
-
+  const login = () => console.log("login");
   //logout
-
+  const logout = () => console.log("logout");
   //clear errors
+  const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 
   return (
     <AuthContext.Provider
@@ -40,6 +80,10 @@ const AuthState = (props) => {
         loading: state.loading,
         user: state.user,
         error: state.error,
+        register,
+        loadUser,
+        login,
+        clearErrors,
       }}
     >
       {props.children}
