21036cf00999435562a7a0cc5e9874efc659ea8a
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-11-02 17:05:40 +0800

    unit testing

---
+++ Unit_Testing/test/test/main_test.js
@@ -0,0 +1,8 @@
+var expect = require("chai").expect;
+// test suit
+describe(" ", function () {
+  // test spec (unit test)
+  it("should ren our tests using npm", function () {
+    expect(true).to.be.ok;
+  });
+});
