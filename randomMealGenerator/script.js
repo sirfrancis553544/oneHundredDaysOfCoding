4296af94410e9a9f5bb00e05198f710827cd3266
sirfrancis553544 <55309898+sirfrancis553544@users.noreply.github.com>
2020-12-09 00:51:34 +0800

    Update script.js

--- randomMealGenerator/script.js
+++ randomMealGenerator/script.js
@@ -22,51 +22,61 @@ const createMeal = (meal) => {
     }
   }
 
-    const newInnerHTML = `
+  const newInnerHTML = `
   <div container>
 
-<div>
-<div class="card">
-<img src="${meal.strMealThumb}" alt="Meal Image">
-</div>
-${
-  meal.strCategory
-    ? `<p><strong>Category:</strong> ${meal.strCategory}</p>`
-    : ""
-}
-${meal.strArea ? `<p><strong>Area:</strong> ${meal.strArea}</p>` : ""}
-${
-  meal.strTags
-    ? `<p><strong>Tags:</strong> ${meal.strTags.split(",").join(",")}</p>`
-    : ""
-}
-<h3>Ingredients:</h3>
-<ul>
-${ingredients.map((ingredient) => `<li>${ingredient}</li>`).join("")}
-</ul>
+        <div>
+             <div class="card">
+                 <img src="${meal.strMealThumb}" alt="Meal Image">
+        </div>
+                ${
+                  meal.strCategory
+                    ? `<p><strong>Category:</strong> ${meal.strCategory}</p>`
+                    : ""
+                }
+                ${
+                  meal.strArea
+                    ? `<p><strong>Area:</strong> ${meal.strArea}</p>`
+                    : ""
+                }
+                ${
+                  meal.strTags
+                    ? `<p><strong>Tags:</strong> ${meal.strTags
+                        .split(",")
+                        .join(",")}</p>`
+                    : ""
+                }
+        <h3>Ingredients:</h3>
+            <ul>
+                 ${ingredients
+                   .map((ingredient) => `<li>${ingredient}</li>`)
+                   .join("")}
+            </ul>
 
 
 
-<div>
-<h4>${meal.strMeal}</h4>
-<p>${meal.strInstructions}</p>
-</div>
-</div>
+        <div>
+            <h4>${meal.strMeal}</h4>
+            <p>${meal.strInstructions}</p>
+        </div>
+        </div>
 
-${
-  meal.strYoutube
-    ? `
-<div class="youtube">
-<h4>Video Recipe</h4>
-<div class="videoWrapper">
-<iframe width="420" height="315"
-src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
-</iframe>
-</div>
-</div>
-`
-    : ""
-}
+             ${
+               meal.strYoutube
+                 ? `
+        <div class="youtube">
+            <h4>Video Recipe</h4>
+         <div class="videoWrapper">
+            <iframe width="420" height="315"
+                 src="https://www.youtube.com/embed/${meal.strYoutube.slice(
+                   -11
+                 )}">
+            </iframe>
+        </div>
+        </div>
+        `
+                 : ""
+             }
 </div>
 
 `;
